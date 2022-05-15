1448f8c2a6affa19f68749e224e0b3900c4b97e3
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-05-14 20:45:24 -0500

    Update JavaScriptPractice.js
    Added an example of a constant array and the things you can do with it

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -51,3 +51,12 @@ let x = 2;
 const PI = 3.141592653589793;
 PI = 3.14;      // This will give an error
 PI = PI + 10;   // This will also give an error
+
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
