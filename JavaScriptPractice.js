a2bf8169bbc03bd1b675d83a392bc744c96fa97f
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-05-15 19:33:37 -0500

    Update JavaScriptPractice.js
    Added an example of a constant object

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -60,3 +60,12 @@ cars[0] = "Toyota";
 
 // You can add an element:
 cars.push("Audi");
+
+// You can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
