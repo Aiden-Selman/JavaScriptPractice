75e35ab7d3377837f065c40c026e0405e8323892
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-05-22 23:22:15 -0500

    Update JavaScriptPractice.js
    Added an example function

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -85,3 +85,9 @@ let z = x * y;
 let length = 16;                               // Number
 let lastName = "Johnson";                      // String
 let x = {firstName:"John", lastName:"Doe"};    // Object
+
+// Functions
+function myFunction(p1, p2) 
+{
+    return p1 * p2;   // The function returns the product of p1 and p2
+}
