4a4821b8f8a6238a2d19603c26efe5827f7d7160
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-05-29 11:25:58 -0500

    Update JavaScriptPractice.js
    Added examples of how to declare strings

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -106,3 +106,8 @@ const person =
 
 // Methods
 name = person.fullName();
+
+// Strings
+// Declartion
+let carName1 = "Volvo XC60";  // Double quotes
+let carName2 = 'Volvo XC60';  // Single quotes
