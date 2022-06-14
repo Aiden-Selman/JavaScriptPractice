6ece1a0d0dac42ba6e7bef33b97265e84b6db38b
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-06-14 10:40:00 -0500

    Update JavaScriptPractice.js
    Added an example of the string method length

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -116,3 +116,8 @@ let carName2 = 'Volvo XC60';  // Single quotes
 let answer1 = "It's alright";
 let answer2 = "He is called 'Johnny'";
 let answer3 = 'He is called "Johnny"';
+
+// Methods
+// Length
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
