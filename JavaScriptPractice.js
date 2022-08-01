f9b371e9148a2dd1fbb726a8e0864a164ee5e448
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-08-01 18:56:36 -0500

    Update JavaScriptPractice.js
    Added an example of the toString method to convert js base 10 numbers to others

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -245,4 +245,15 @@ let y = -2 / 0;
 typeof Infinity;
 
 // Hexadecimal
+// Declaration
 let x = 0xFF;
+
+// toString
+// By default, JavaScript displays numbers as base 10 decimals. But you can use the toString() method to output numbers from base 2 to base 36. Hexadecimal is base 16. Decimal is base 10. Octal is base 8. Binary is base 2.
+let myNumber = 32;
+myNumber.toString(32);
+myNumber.toString(16);
+myNumber.toString(12);
+myNumber.toString(10);
+myNumber.toString(8);
+myNumber.toString(2);
