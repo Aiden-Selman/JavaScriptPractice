25cb21ef269a0db53312dce5c00b9e397b16ad32
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-08-06 22:29:23 -0500

    Update JavaScriptPractice.js
    Added an example of how to use the toFixed number method

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -268,3 +268,11 @@ let x = 9.656;
 x.toExponential(2);
 x.toExponential(4);
 x.toExponential(6);
+
+// toFixed
+// toFixed() returns a string, with the number written with a specified number of decimals
+let x = 9.656;
+x.toFixed(0);
+x.toFixed(2);
+x.toFixed(4);
+x.toFixed(6);
